48e647efec38f95424ee8666cd7e45de61b76e3b
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-10-11 21:55:45 +0800

    Add files via upload

---
+++ commom.c
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+
+//int main()
+//{ 
+//	float i = 80.834;
+//	double j =80.834;
+//	printf("%.2f\n",(int)i);
+//	printf("%.2lf",(int)j);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int i = 1234;
+//	printf("%#o ", i);
+//	printf("%#x", i);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int ret= printf("Hello world!");
+//	
+//	printf("%d\n", ret);
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	int a, s, d, f, g, h, j, k, l, z, x, c;
+//	scanf("%d, %d, %d, %d, %d, %d, %d, %d, %d, %d, %d , %d", &a, &s, &d, &f, &g, &h, &j, &k, &l, &z, &x, &c);
+//	printf("%c%c%c%c%c%c%c%c%c%c%c%c", (char)a, (char)s, (char)d, (char)f, (char)g, (char)h, (char)j, (char)k, (char)l, (char)z, (char)x, (char)c);
+//	return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int i;
+//	scanf("%d", &i);
+//long long j = i * 31560000;
+//	printf("%d",( j);
+//	return 0;
+//}
