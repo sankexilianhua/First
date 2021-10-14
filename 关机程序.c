5a2720694682f5dd74e2af87cd67e1220e8f4abb
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-10-14 23:26:37 +0800

    Add files via upload

---
+++ 关机程序.c
@@ -0,0 +1,26 @@
+#define  _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//#include<stdlib.h>
+//#include<string.h>
+//#include<windows.h>
+//int main()
+//{
+//	system("shutdown -s -t 60");
+//     while (1)
+//	{
+//		 printf("你的电脑将在60秒后关机，请输入我是猪，取消关机\n");
+//		 char arr[10];
+//		 scanf("%s", &arr);
+//		 if (strcmp(arr, "我是猪") == 0)
+//		 {
+//			 system("shutdown -a");
+//			 printf("你的电脑已取消关机。\n");
+//			 break;
+//		 }
+//		 else
+//			 printf("在给你一次机会好好想想\n");
+//	}
+//
+//		return 0;
+//}
+
