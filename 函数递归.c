cfb938fd5a84f28dead077fff7ed29e8d1fcc6f9
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-10-15 23:59:52 +0800

    Add files via upload

---
+++ 函数递归.c
@@ -0,0 +1,28 @@
+//#define  _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+////ÇónµÄ½×³Ë
+//int Fac1(int n)
+//{
+//	int i = 0;
+//	int ret = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		 ret *= i;
+//	}
+//	return ret;
+//}
+//int Fac2(int n)
+//{
+//	if (n <= 1)
+//		return 1;
+//	else
+//		return n*Fac2(n - 1);
+//}
+//int main()
+//{
+//	int n;
+//	scanf("%d", &n);
+//	int ret1 = Fac1(n);
+//	int ret2 = Fac2(n);
+//	return 0;
+//}
