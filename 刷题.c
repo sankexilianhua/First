50cb46d542f1df45d41ff8ea2087558a2f2a2b62
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-10-16 00:00:04 +0800

    Add files via upload

---
+++ 刷题.c
@@ -0,0 +1,39 @@
+//#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+//void sort(int arr[], int n)
+//{
+//	int i = 0;
+//	for (; i<n - 1; i++)
+//	{
+//		int j = i;
+//		for (; j<n - 1; j++)
+//		{
+//			if (arr[j]<arr[j + 1])
+//			{
+//				j = j + 1;
+//			}
+//		}
+//		int tmp = arr[j];
+//		arr[j] = arr[i];
+//		arr[i] = tmp;
+//	}
+//
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	int arr[40] = { 0 };
+//	int i = 0;
+//	//for (i = 0; i<n; i++)
+//	//{
+//		//scanf("%d", arr[i]);
+//	//}
+//	scanf("%d %d %d %d %d", arr[0], arr[1], arr[2], arr[3], arr[4]);
+//	sort(arr, n);
+//	for (i = 0; i<5; i++)
+//	{
+//		printf("%d ", arr[i]);
+//	}
+//	return 0;
+//}
