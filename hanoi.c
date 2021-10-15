a6e1100e709f9e86749592e0df085ab821c832bf
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-10-16 00:00:20 +0800

    Add files via upload

---
+++ hanoi.c
@@ -0,0 +1,22 @@
+#define  _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+void Hanoi(int n, char A, char B, char C)
+{
+	if (n == 1)
+		printf("%c->%c\n",A,C);
+	else
+	{
+		Hanoi(n - 1, A, C, B);
+		printf("%c->%c\n",A,C);
+		Hanoi(n - 1, B, A, C);
+	}
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	printf("要移动的盘子数：%d\n",n);
+	printf("实现过程：\n");
+	Hanoi(n, 'A',' B', 'C');
+	return 0;
+}
