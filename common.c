a7e4eed9f2e0174aa230222d06d5208cd50d9fa0
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-10-22 22:00:06 +0800

    Add files via upload

--- common.c
+++ common.c
@@ -1,43 +1,40 @@
 #define  _CRT_SECURE_NO_WARNINGS
+//#include<stdio.h>
+//int main()
+//{
+//	int n;
+//	while (~scanf("%d", &n))
+//	{
+//		int i, j;
+//		for (i = n; i >= 1; i--)
+//		{
+//			for (j = 1; j <= n; j++)
+//			{
+//				if (j >= i)
+//					printf("* ");
+//				else
+//					printf("  ");
+//			}
+//			printf("\n");
+//		}
+//	}
+//	return 0;
+//}
+
 #include<stdio.h>
 int main()
 {
-	double i, j;
-	char ch;
-	while (scanf("%lf%c%lf", &i, &ch, &j) != EOF)
+	int n;
+	while (~scanf("%d", &n))
 	{
-		switch (ch)
+		int i, j;
+		for (i = n; i >= 1; i--)
 		{
-		case'+':
-		{
-				   printf("%.4lf%c%.4lf=%.4lf\n", i, ch, j, i + j);
-				   break; }
-		case'-':
-		{
-				   printf("%.4lf%c%.4lf=%.4lf\n", i, ch, j, i - j);
-				   break; }
-		case'*':
-		{
-				   printf("%.4lf%c%.4lf=%.4lf\n", i, ch, j, i*j);
-				   break; }
-		case'/':
-		{
-				   if (j == 0)
-				   {
-					   printf("Wrong!Division by zero!\n");
-					   break;
-				   }
-				   else
-				   {
-					   printf("%.4lf%c%.4lf=%.4lf\n", i, ch, j, i / j);
-					   break;
-				   }
-		}
-		default:
-		{
-				   printf("Invalid operation!\n");
-				   break;
-		}
+			for (j = 0; j<i; j++)
+			{
+				printf("* ");
+			}
+			printf("\n");
 		}
 	}
 	return 0;
