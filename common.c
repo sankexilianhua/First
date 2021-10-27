a481573c0c149b9e1bf244dadee90582b4534305
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-10-27 23:50:29 +0800

    Add files via upload

--- common.c
+++ common.c
@@ -3,75 +3,43 @@
 //int main()
 //{
 //	int n;
-//	scanf("%d", &n);
-//	int i = 0;
-//	int m = n;
-//	for (i = 1; i <= m; i++)
-//	{
-//		int j = 0;
-//		n = m;
-//		for (j = 0; j<i; j++)
-//		{
-//			
-//			for (n=n-i+1; n>1; n--)
-//			{
-//				printf(" ");
-//			}
-//			printf("* ");
-//			if (j == i - 1)
-//				printf("\n");
-//		}
-//
-//	}
-//	return 0;
-//}
-//#include<stdio.h>
-//int main(){
-//	int n = 0;
-//	while (~scanf("%d\n", &n)){
-//		int i = 0;
-//		for (i = 0; i<n; i++){
-//			int j = 0;
-//			for (j = 0; j<n - i - 1; j++){
-//				printf(" ");
-//			}
-//			for (j = 0; j <= i; j++){
-//				printf("* ");
-//			}
-//			printf("\n");
-//		}
-//	}
-//	return 0;
-//}
-//#include<stdio.h>
-//int main()
-//{
-//	int n;
 //	while (~scanf("%d", &n))
 //	{
 //		int i = 0;
-//		for (i = 1; i <= n; i++)
+//		for (i = n; i >= 0; i--)
 //		{
 //			int j = 0;
-//			for (j = 0; j<n - i + 1; j++)
-//				printf(" ");
-//			for (j = 0; j<i; j++)
-//				printf("* ");
+//			if (i>0)
+//			{
+//				for (j = i; j>0; j--)
+//				{
+//					printf("  ");
+//				}
+//			}
+//			for (j = n; j>i - 1; j--)
+//			{
+//				printf("*");
+//			}
 //			printf("\n");
 //		}
-//		for (i = 0; i <= n; i++)
-//			printf("* ");
-//		printf("\n");
 //		for (i = n; i>0; i--)
 //		{
 //			int j = 0;
-//			for (j = 0; j<i - 1; j++)
-//				printf(" ");
-//			for (j = 0; j<i; j++)
-//				printf("* ");
+//			for (j = n; j >= i; j--)
+//				printf("  ");
+//			for (j = i; j>0; j--)
+//				printf("*");
 //			printf("\n");
 //		}
 //	}
+//	return 0;
+//}
+
+//#include<stdio.h>
+//int main()
+//{
+//	char ch = 'A';
+//	printf("%c\n",ch);
 //	return 0;
 //}
 
@@ -82,27 +50,32 @@ int main()
 	while (~scanf("%d", &n))
 	{
 		int i = 0;
-		for (i = 1; i <= n; i++)
-		{
-			int j = 0;
-			for (j = 0; j<n - i + 1; j++)
-				printf(" ");
-			for (j = 0; j<i; j++)
-				printf("* ");
-			printf("\n");
-		}
-		for (i = 0; i <= n; i++)
-			printf("* ");
-		printf("\n");
 		for (i = 0; i<n; i++)
 		{
 			int j = 0;
-			for (j = n; j>n - i - 1; j--)
-				printf(" ");
-			for (j = n; j>i; j--)
-				printf("* ");
+			if (i == 0)
+			{
+				for (j = 0; j<n; j++)
+					printf("* ");
+			}
+			else if (i == n - 1)
+			{
+				for (j = 0; j<n; j++)
+					printf("* ");
+			}
+			else
+			{
+				for (j = 0; j<n; j++)
+				{
+					if (j == 0 || j == n - 1)
+						printf("*");
+					else
+						printf(" ");
+				}
+			}
 			printf("\n");
 		}
+
 	}
 	return 0;
 }
