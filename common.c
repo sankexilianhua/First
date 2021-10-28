8f7b4111ba8d49cfbcb2612e458d72dda2c93d97
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-10-28 23:24:07 +0800

    Add files via upload

--- common.c
+++ common.c
@@ -1,81 +1,120 @@
 #define  _CRT_SECURE_NO_WARNINGS
+//用*打印出打印x型
+//例子：
+//*   *
+// * *
+//  *
+// * *
+//*   *
+
 //#include<stdio.h>
 //int main()
 //{
 //	int n;
 //	while (~scanf("%d", &n))
 //	{
-//		int i = 0;
-//		for (i = n; i >= 0; i--)
+//		int i;
+//		if (n % 2 != 0)
 //		{
-//			int j = 0;
-//			if (i>0)
+//			int l = 1, r = n;
+//			for (i = 0; i<n / 2 + 1; i++)
 //			{
-//				for (j = i; j>0; j--)
+//				int a = 1, e = 0;
+//				for (e = 0, a = 1; e<n; e++, a++)
 //				{
-//					printf("  ");
+//					if (a == l || a == r)
+//						printf("*");
+//					else
+//						printf(" ");
 //				}
+//				l++;
+//				r--;
+//				printf("\n");
 //			}
-//			for (j = n; j>i - 1; j--)
+//			l = l - 2, r = r + 2;
+//			for (i = 0; i<n / 2; i++)
 //			{
-//				printf("*");
+//				int a = 1, e = 0;
+//				for (e = 0, a = 1; e<n; e++, a++)
+//				{
+//					if (a == l || a == r)
+//						printf("*");
+//					else
+//						printf(" ");
+//				}
+//				l--;
+//				r++;
+//				printf("\n");
 //			}
-//			printf("\n");
 //		}
-//		for (i = n; i>0; i--)
+//		else
 //		{
-//			int j = 0;
-//			for (j = n; j >= i; j--)
-//				printf("  ");
-//			for (j = i; j>0; j--)
-//				printf("*");
-//			printf("\n");
+//			int l = 1, r = n;
+//			for (i = 0; i<n / 2; i++)
+//			{
+//				int a = 1, e = 0;
+//				for (e = 0, a = 1; e<n; e++, a++)
+//				{
+//					if (a == l || a == r)
+//						printf("*");
+//					else
+//						printf(" ");
+//				}
+//				l++;
+//				r--;
+//				printf("\n");
+//			}
+//			l = l - 1, r = r + 1;
+//			for (i = 0; i<n / 2; i++)
+//			{
+//				int a = 1, e = 0;
+//				for (e = 0, a = 1; e<n; e++, a++)
+//				{
+//					if (a == l || a == r)
+//						printf("*");
+//					else
+//						printf(" ");
+//				}
+//				l--;
+//				r++;
+//				printf("\n");
+//			}
 //		}
 //	}
 //	return 0;
 //}
-
 //#include<stdio.h>
 //int main()
 //{
-//	char ch = 'A';
-//	printf("%c\n",ch);
+//	int k = 0;
+//	while (scanf("%d", &k) != EOF)
+//	{
+//		int i = 0;
+//		for (i = 0; i < k; i++)
+//		{
+//			int j = 0;
+//			for (j = 0; j < k; j++)
+//			{
+//				if (j == i)
+//					printf("*");
+//				else if (j + i == k - 1)
+//					printf("*");
+//				else
+//					printf(" ");
+//			}
+//			printf("\n");
+//		}
+//	}
+//
 //	return 0;
 //}
 
 #include<stdio.h>
 int main()
 {
-	int n;
-	while (~scanf("%d", &n))
-	{
-		int i = 0;
-		for (i = 0; i<n; i++)
-		{
-			int j = 0;
-			if (i == 0)
-			{
-				for (j = 0; j<n; j++)
-					printf("* ");
-			}
-			else if (i == n - 1)
-			{
-				for (j = 0; j<n; j++)
-					printf("* ");
-			}
-			else
-			{
-				for (j = 0; j<n; j++)
-				{
-					if (j == 0 || j == n - 1)
-						printf("*");
-					else
-						printf(" ");
-				}
-			}
-			printf("\n");
-		}
+	float arr[5] = { 0 };
 
-	}
+	for (int i = 0; i < 5; i++)
+		printf("%f", arr[i]);
 	return 0;
 }
