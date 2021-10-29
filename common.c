3c65e17b8c64489293d3c085bbc281d13b19937a
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-10-29 23:22:39 +0800

    Add files via upload

--- common.c
+++ common.c
@@ -1,120 +1,47 @@
 #define  _CRT_SECURE_NO_WARNINGS
-//用*打印出打印x型
-//例子：
-//*   *
-// * *
-//  *
-// * *
-//*   *
-
-//#include<stdio.h>
-//int main()
-//{
-//	int n;
-//	while (~scanf("%d", &n))
-//	{
-//		int i;
-//		if (n % 2 != 0)
-//		{
-//			int l = 1, r = n;
-//			for (i = 0; i<n / 2 + 1; i++)
-//			{
-//				int a = 1, e = 0;
-//				for (e = 0, a = 1; e<n; e++, a++)
-//				{
-//					if (a == l || a == r)
-//						printf("*");
-//					else
-//						printf(" ");
-//				}
-//				l++;
-//				r--;
-//				printf("\n");
-//			}
-//			l = l - 2, r = r + 2;
-//			for (i = 0; i<n / 2; i++)
-//			{
-//				int a = 1, e = 0;
-//				for (e = 0, a = 1; e<n; e++, a++)
-//				{
-//					if (a == l || a == r)
-//						printf("*");
-//					else
-//						printf(" ");
-//				}
-//				l--;
-//				r++;
-//				printf("\n");
-//			}
-//		}
-//		else
-//		{
-//			int l = 1, r = n;
-//			for (i = 0; i<n / 2; i++)
-//			{
-//				int a = 1, e = 0;
-//				for (e = 0, a = 1; e<n; e++, a++)
-//				{
-//					if (a == l || a == r)
-//						printf("*");
-//					else
-//						printf(" ");
-//				}
-//				l++;
-//				r--;
-//				printf("\n");
-//			}
-//			l = l - 1, r = r + 1;
-//			for (i = 0; i<n / 2; i++)
-//			{
-//				int a = 1, e = 0;
-//				for (e = 0, a = 1; e<n; e++, a++)
-//				{
-//					if (a == l || a == r)
-//						printf("*");
-//					else
-//						printf(" ");
-//				}
-//				l--;
-//				r++;
-//				printf("\n");
-//			}
-//		}
-//	}
-//	return 0;
-//}
-//#include<stdio.h>
-//int main()
-//{
-//	int k = 0;
-//	while (scanf("%d", &k) != EOF)
-//	{
-//		int i = 0;
-//		for (i = 0; i < k; i++)
-//		{
-//			int j = 0;
-//			for (j = 0; j < k; j++)
-//			{
-//				if (j == i)
-//					printf("*");
-//				else if (j + i == k - 1)
-//					printf("*");
-//				else
-//					printf(" ");
-//			}
-//			printf("\n");
-//		}
-//	}
-//
-//	return 0;
-//}
-
 #include<stdio.h>
+#include<stdlib.h>
+#include<windows.h>
+int Add()
+{
+	float a, x, y;
+	for (y = 1.5f; y>-1.5f; y -= 0.1f)
+	{
+		for (x = -1.5f; x<1.5f; x += 0.05f)
+		{
+			a = x*x + y*y - 1;
+			//这里的@符号即为打印出的心形图案符号，可更改
+			char ch = a*a*a - x*x*y*y*y <= 0.0f ? '@' : ' ';
+			putchar(ch);
+			//或者putchar(a*a*a-x*x*y*y*y<=0.0f?'*':' ');
+		}
+		printf("\n");
+	}
+}
 int main()
 {
-	float arr[5] = { 0 };
+	int(*pa)() = Add;
+	printf("%d\n",(*pa)());
+	for (int i = 0; i < 10000; i++)
+	{
+		system("color 1");
+		Sleep(500);
+		system("color 2");
+		Sleep(500);
+		system("color 3");
+		Sleep(500);
+		system("color 4");
+		Sleep(500);
+		system("color 5");
+		Sleep(500);
+		system("color 6");
+		Sleep(500);
+		system("color 7");
+		Sleep(500);
+		system("color 8");
+		Sleep(500);
 
-	for (int i = 0; i < 5; i++)
-		printf("%f", arr[i]);
+	}
+	system("pause");
 	return 0;
 }
