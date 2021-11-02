b35b0db810c8c3c056210c86f0429e745548e901
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-11-02 23:23:32 +0800

    Add files via upload

--- common.c
+++ common.c
@@ -1,71 +1,18 @@
 #define  _CRT_SECURE_NO_WARNINGS
-#define PI 3.1415926
 #include<stdio.h>
+int com_int(void* e1, void* e2)
+{
+	return *(int*)e1 - *(int*)e2;
+}
 int main()
 {
-	int a, b, c, d, e, f, g;
-	scanf("%d%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f, &g);
-	int tmp;
-	if (a<b)
+	int arr[10] = {9,8,7,6,5,4,3,2,1,0};
+	int sz = sizeof(arr) / sizeof(arr[0]);
+	qsort(arr,sz,sizeof(arr[0]),com_int);
+	int i = 0;
+	for (; i < 10; i++)
 	{
-		tmp = a;
-		a = b;
-		b = tmp;
+		printf("%d ",arr[i]);
 	}
-	if (a<c)
-	{
-		tmp = a;
-		a = c;
-		c = tmp;
-	} if (a<d)
-	{
-		tmp = a;
-		a = d;
-		d = tmp;
-	} if (a<e)
-	{
-		tmp = a;
-		a = e;
-		e = tmp;
-	} if (a<f)
-	{
-		tmp = a;
-		a = f;
-		f = tmp;
-	}
-	if (g>a)
-	{
-		tmp = a;
-		a = g;
-		g = tmp;
-	}
-	if (g>b)
-	{
-		tmp = b;
-		b = g;
-		g = tmp;
-	}    if (g>c)
-	{
-		tmp = c;
-		c = g;
-		g = tmp;
-	}    if (g>d)
-	{
-		tmp = d;
-		d = g;
-		g = tmp;
-	}    if (g>e)
-	{
-		tmp = e;
-		e = g;
-		g = tmp;
-	}    if (g>f)
-	{
-		tmp = f;
-		f = g;
-		g = tmp;
-	}
-	double sum = (b + c + d + e + f) / 5.0;
-	printf("%.2lf", sum);
 	return 0;
 }
