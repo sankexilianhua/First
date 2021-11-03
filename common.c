79bb3f690aed9eb7a16c69d7f3b2dfc938aa6e05
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-11-03 15:21:25 +0800

    Add files via upload

--- common.c
+++ common.c
@@ -1,18 +1,16 @@
 #define  _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-int com_int(void* e1, void* e2)
-{
-	return *(int*)e1 - *(int*)e2;
-}
 int main()
 {
-	int arr[10] = {9,8,7,6,5,4,3,2,1,0};
-	int sz = sizeof(arr) / sizeof(arr[0]);
-	qsort(arr,sz,sizeof(arr[0]),com_int);
-	int i = 0;
-	for (; i < 10; i++)
+	int n;
+	while (~scanf("%d", &n))
 	{
-		printf("%d ",arr[i]);
+		long long int sum = 0, i;
+		for (i = 1; i <= n; i++)
+		{
+			sum += i;
+		}
+		printf("%d\n\n",sum);
 	}
 	return 0;
 }
