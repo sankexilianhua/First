768c1ed23a1f28f0b7e7a18048cab82256f6bde3
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-11-08 22:38:06 +0800

    Add files via upload

--- common_11_7.c
+++ common_11_7.c
@@ -14,48 +14,194 @@
 //	else
 //		return false;
 //}
-int findMiddleIndex(int* nums, int numsSize)
+//int findMiddleIndex(int* nums, int numsSize)
+//{
+//	int midleft = 0;
+//	int i = 0, j = numsSize;
+//	int rightsum = 0, leftsum = 0;
+//	for (midleft = 0; midleft<numsSize; midleft++)
+//	{
+//		rightsum = 0;
+//		leftsum = 0;
+//		for (j = numsSize - 1; j>midleft; j--)
+//		{
+//			if (midleft == numsSize - 1)
+//			{
+//				leftsum = 0;
+//			}
+//			else
+//				rightsum += nums[j];
+//		}
+//		for (i = 0; i <= midleft; i++)
+//		{
+//			if (midleft == 0)
+//			{
+//				leftsum = 0;
+//			}
+//			else
+//				leftsum += nums[i];
+//		}
+//		if (leftsum == rightsum)
+//		{
+//			break;
+//		}
+//	}
+//	if (midleft<numsSize)
+//		return midleft;
+//	else
+//		return -1;
+//}
+//int main()
+//{
+//	int nums[] = { 2, 3, -1, 8, 4 };
+//	int sz = sizeof(nums) / sizeof(nums[0]);
+//	int ret =   findMiddleIndex(nums, sz);
+//	printf("%d",ret);
+//	return 0;
+//}
+
+//int hammingWeight(uint32_t n)
+//{
+//	int count = 0;
+//	int i = 1;
+//	while (n != 0)
+//	{
+//		if (n^i == 0)
+//		{
+//			count++;
+//		}
+//		i << 1;
+//	}
+//	return count;
+//}
+
+//int addDigits(int num)
+//{
+//	int ret[10000] = { 0 };
+//	int i, count = 1;
+//	while (1)
+//	{
+//		i = 0;
+//		while (num)
+//		{
+//			ret[i] = num % 10;
+//			num /= 10;
+//			i++;
+//			count++;
+//		}
+//		for (i = 0; i<count; i++)
+//			num += ret[i];
+//		if (num<10)
+//			break;
+//	}
+//	return num;
+//
+//}
+/*
+int sumBase(int n, int k)
 {
-	int midleft = 0;
-	int i = 0, j = numsSize;
-	int rightsum = 0, leftsum = 0;
-	for (midleft = 0; midleft<numsSize; midleft++)
+	int ret[10000] = { 0 };
+	int i = 0;
+	int count = 0;
+	int sum = 0;
+	while (n)
 	{
-		rightsum = 0;
-		leftsum = 0;
-		for (j = numsSize - 1; j>midleft; j--)
-		{
-			if (midleft == numsSize - 1)
-			{
-				leftsum = 0;
-			}
-			else
-				rightsum += nums[j];
-		}
-		for (i = 0; i <= midleft; i++)
-		{
-			if (midleft == 0)
-			{
-				leftsum = 0;
-			}
-			else
-				leftsum += nums[i];
-		}
-		if (leftsum == rightsum)
-		{
-			break;
-		}
+		ret[i] = n%k;
+		n = n / k;
+		i++;
 	}
-	if (midleft<numsSize)
-		return midleft;
-	else
-		return -1;
+	for (; i + 1>0; i--, count++)
+	{
+		sum += (i * 10 * ret[count]);
+	}
+	//return sum;
+}*/
+
+//char ret[100] = { 0 };
+//char * convertToBase7(int num)
+//{
+//	int i = 0;
+//	if (num<0)
+//	{
+//		ret[0] = '-';
+//		i++;
+//	}
+//	num = abs(num);
+//	while (num)
+//	{
+//		if (num % 7 == 0 && num / 7 == 1)
+//			ret[i] = '1';
+//		else
+//			ret[i] = num % 7 + '0';
+//		num /= 7;
+//		i++;
+//	}
+//	ret[i + 1] = '\0';
+//	return ret;
+//}
+
+char ret[100] = { '0' };
+void reverse(char* ret , int i)
+{
+	int left = 1;
+	int right = i - 1;
+	while (left <= right)
+	{
+		char tmp = ret[left];
+		ret[left] = ret[right];
+		ret[right] = tmp;
+		left++;
+		right--;
+	}
+}
+char * convertToBase7(int num)
+{
+	int i = 0;
+	int j = num;
+	if (num<0)
+	{
+		ret[0] = '-';
+		i++;
+	}
+	int count = 1;
+	num = abs(num);
+	while (num)
+	{
+		ret[i] = num % 7 + '0';
+		num /= 7;
+		i++;
+	}
+	if (j<0)
+		reverse(ret, i);
+	ret[i + 1] = '\0';
+	return ret;
 }
 int main()
 {
-	int nums[] = { 2, 3, -1, 8, 4 };
-	int sz = sizeof(nums) / sizeof(nums[0]);
-	int ret =   findMiddleIndex(nums, sz);
-	printf("%d",ret);
+	int n = 2;
+	convertToBase7(n);
+	printf("%s",ret);
 	return 0;
 }
+
+//char ret[100] = { 0 };
+//char * convertToBase7(int num)
+//{
+//	int i = 0;
+//	if (num<0)
+//	{
+//		ret[0] = '-';
+//		i++;
+//	}
+//	num = abs(num);
+//	while (num != 1)
+//	{
+//		ret[i] = num % 7 + '0';
+//		num /= 7;
+//		i++;
+//	}
+//	if (num == 1)
+//		i++;
+//	ret[i + 1] = '\0';
+//	return ret;
+//}
