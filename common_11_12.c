91d3f322ef0c78cd9413b7633badc208e1118c5a
sankexilianhua <92084058+sankexilianhua@users.noreply.github.com>
2021-11-12 22:49:14 +0800

    Add files via upload

---
+++ common_11_12.c
@@ -0,0 +1,55 @@
+#define  _CRT_SECURE_NO_WARNINGS
+
+if (i >= 10 && m<0)
+{
+	if (arr[0]>2)
+		sum = 0;
+	else
+	{
+		if (arr[1]>1)
+			sum = 0;
+		else
+		{
+			if (arr[2]>4)
+				sum = 0;
+			else
+			{
+				if (arr[3]>7)
+					sum = 0;
+				else
+				{
+					if (arr[4]>4)
+						sum = 0;
+					else
+					{
+						if (arr[5]>8)
+							sum = 0;
+						else   //2147483647
+						{
+							if (arr[6]>3)
+								sum = 0;
+							else
+							{
+								if (arr[7]>6)
+									sum = 0;
+								else
+								{
+									if (arr[8]>4)
+										sum = 0;
+									else
+									{
+										if (arr[9]>7)
+											sum = 0;
+										else
+											;
+									}
+								}
+							}
+						}
+					}
+				}
+			}
+		}
+
+	}
+}
